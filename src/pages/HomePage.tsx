aa90605b6ade9f2c4200dcce99c879bcea1d5962
Martin Vad <90182227+dietzy1@users.noreply.github.com>
2023-05-02 23:00:47 +0200

    homepage cleanup

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,17 +1,11 @@
 /** @format */
-
-import logo from "../assets/logo.svg";
-
 import React, { useMemo } from "react";
 import "../App.css";
 import { defaultGlobalStateType, useGlobalState } from "../context/context";
-import { Navigate } from "react-router-dom";
+
 import { useState, useEffect, useCallback, useRef } from "react";
 
-//ICONS
-import { ArrowLeftOnRectangleIcon } from "@heroicons/react/24/outline";
-import { UserCircleIcon } from "@heroicons/react/24/outline";
-import { Cog8ToothIcon } from "@heroicons/react/24/outline";
+import useModalState from "../hooks/useModalState";
 
 //API services
 import { ChatroomGatewayService } from "../api/protos/chatroom/v1/chatroomgateway_service_connect";
@@ -21,10 +15,7 @@ import { AccountGatewayService } from "../api/protos/account/v1/accountgateway_s
 import { AuthGatewayService } from "../api/protos/auth/v1/authgateway_service_connect";
 
 //API requests
-import {
-  Activity,
-  GetRoomRequest,
-} from "../api/protos/chatroom/v1/chatroomgateway_service_pb";
+import { Activity } from "../api/protos/chatroom/v1/chatroomgateway_service_pb";
 import { GetRoomResponse } from "../api/protos/chatroom/v1/chatroomgateway_service_pb";
 
 //COMPONENTS
@@ -32,27 +23,17 @@ import Activitybar from "../components/Activitybar";
 import Navbar from "../components/Navbar";
 
 import Chatroombar from "../components/Chatroombar";
-import Channel from "../components/chatroomComponents/Channel";
+
 import Footer from "../components/Footer";
 import Chat from "../components/Chat";
 
 import { useNavigate } from "react-router-dom";
 
-//PORTALS
-import { openAccountFunc, openSettingsFunc } from "../portals/Openportals";
-import Account from "../portals/Account";
-import Settings from "../portals/Settings";
-
 //WEBSOCKET
 import useWebSocket, { ReadyState } from "react-use-websocket";
 import { Unmarshal, decodeMessage } from "../websocket/Serialize";
 import { Client } from "../api/Client";
-import {
-  AuthenticateRequest,
-  AuthenticateResponse,
-  LogoutRequest,
-  LogoutResponse,
-} from "../api/protos/auth/v1/authgateway_service_pb";
+
 import {
   GetUserRequest,
   GetUserResponse,
@@ -71,39 +52,24 @@ import {
   Msg,
 } from "../api/protos/message/v1/messagegateway_service_pb";
 import { options } from "../websocket/options";
+import { useAuthentication } from "../hooks/useAuthentication";
+
+//PORTALS (MODALS)
+import AccountModal from "../portals/AccountModal";
+import UserModal from "../portals/UserModal";
+import CreateChatroomModal from "../portals/CreateChatroomModal";
 
 export function HomePage() {
+  const authClient = new Client(AuthGatewayService);
+  const userClient = new Client(UserGatewayService);
+
+  //useEffect hook to handle authentication and redirecting to login page if not authenticated
+  useAuthentication(authClient);
+
   //State containing user ID
   const [userState, setUserState] = useState<GetUserResponse | undefined>(
     undefined
   );
-
-  const authClient = new Client(AuthGatewayService);
-  const userClient = new Client(UserGatewayService);
-
-  const [state, dispatch] = useGlobalState();
-  const navigate = useNavigate();
-
-  //Request authentication
-  //useEffect hook to handle authentication and redirecting to login page if not authenticated
-  useEffect(() => {
-    console.log("Authenticating...");
-    (async function () {
-      const req = new AuthenticateRequest();
-      const response = (await authClient.fetch(req)) as
-        | AuthenticateResponse
-        | undefined;
-      if (response !== undefined) {
-        console.log("Authenticated!");
-        navigate("/");
-        dispatch({ user: true });
-      } else {
-        console.log("Not authenticated!");
-        navigate("/login");
-        dispatch({ user: false });
-      }
-    })();
-  }, []);
 
   useEffect(() => {
     console.log("Requesting user information...");
@@ -179,8 +145,15 @@ export function Home({ userState }: { userState: GetUserResponse }) {
   const authClient = new Client(AuthGatewayService);
 
   //Portals
-  const [openAccount, setOpenAccount] = React.useState(false);
-  const [openSettings, setOpenSettings] = React.useState(false);
+  //const [openAccount, setOpenAccount] = React.useState(false);
+  //const [openSettings, setOpenSettings] = React.useState(false);
+
+  /*   const [portals, setPortals] = React.useState({
+    openUser: false,
+    openAccount: false,
+    openCreateChatroom: false,
+    openCreateChannel: false,
+  }); */
 
   const [socketUrl, setSocketUrl] = useState(
     "ws://localhost:8000/ws?" +
@@ -267,21 +240,6 @@ export function Home({ userState }: { userState: GetUserResponse }) {
     })();
   }, [setChatRoomState, chatroomState]);
 
-  /* useEffect(() => {
-    if (typeof channel === "undefined") {
-      return;
-    }
-    if (typeof chatroom === "undefined") {
-      return;
-    }
-    //TODO: This is a temporary solution to the problem of the websocket not connecting
-    setChatroom(chatroomState?.rooms[0].chatroomUuid!);
-    setChannel(chatroomState?.rooms[0].channel[0].channelUuid!);
-
-    console.log("Channel set to: ", channel);
-    console.log("Chatroom set to: ", chatroom);
-  }, [channel, chatroom]);  */
-
   //Start listening for new messages
   useEffect(() => {
     if (lastMessage !== null) {
@@ -321,11 +279,6 @@ export function Home({ userState }: { userState: GetUserResponse }) {
         }
       };
     }
-    /* ref!.current!.scrollIntoView({
-      behavior: "smooth",
-      block: "start",
-      inline: "nearest",
-    }); */
   }, [lastMessage, setMessageState, setActivityState]);
 
   //UseEffect hook that updates the socket url when the chatroom or channel is changed
@@ -399,12 +352,6 @@ export function Home({ userState }: { userState: GetUserResponse }) {
     console.log("Changed socket URL to:", socketUrl);
   }, [chatroom, channel]);
 
-  //This function is used to send messages to the websocket
-  /*   const handleClickSendMessage = useCallback(
-    (msg: Uint8Array) => sendMessage(msg, true),
-    []
-  ); */
-
   const handleClickSendMessage = useCallback(
     (msg: Uint8Array) => {
       console.log(readyState);
@@ -429,6 +376,14 @@ export function Home({ userState }: { userState: GetUserResponse }) {
     },
     [readyState, sendMessage]
   );
+
+  //FIXME: FIXME: FIXME: FIXME:
+  const [portals, openModal, closeModal] = useModalState({
+    openUser: false,
+    openAccount: false,
+    openCreateChatroom: false,
+    openCreateChannel: false,
+  });
 
   return (
     <div className="h-screen w-screen bg-spotify3 flex flex-row justify-between max-h-screen">
@@ -455,6 +410,8 @@ export function Home({ userState }: { userState: GetUserResponse }) {
         user={userState!}
         channeluuid={channel}
         chatroomuuid={chatroom}
+        openModal={openModal}
+        closeModal={closeModal}
       />
 
       <div className="w-[32rem] z-20">
@@ -463,11 +420,13 @@ export function Home({ userState }: { userState: GetUserResponse }) {
             chatroomsState={chatroomState}
             setChatroom={setChatroom}
             setChannel={setChannel}
+            //FIXME:
+            openModal={openModal}
+            closeModal={closeModal}
           />
         )}
       </div>
 
-      {/*I might want to wrap this in a div with collum to fix the ref dummy issue*/}
       <div
         className={` sm:w-full w-full flex justify-center overflow-x-hidden  h-[96vh] overflow-y-scroll scrollbar-hide `}
       >
@@ -483,7 +442,6 @@ export function Home({ userState }: { userState: GetUserResponse }) {
           <div ref={ref}></div>
         </div>
       </div>
-      {/*I might want to wrap this in a div with collum to fix the ref dummy issue*/}
 
       <div className="sm:w-[14rem] h-screen hidden sm:flex flex-col overflow-y-scroll  scrollbar-hide pt-28 bg-spotify3 shadow-inner">
         {/*  <div className=" flex flex-row sm:visible invisible pb-1 mb-4"></div> */}
@@ -497,17 +455,19 @@ export function Home({ userState }: { userState: GetUserResponse }) {
       </div>
 
       {/*REACT PORTALS */}
-      <Account open={openAccount} onClose={() => setOpen(false)} />
-      <Settings open={openSettings} onClose={() => setOpen(false)} />
+
+      <CreateChatroomModal
+        open={portals.openCreateChatroom}
+        onClose={() => closeModal("openCreateChatroom")}
+      />
+      <AccountModal
+        open={portals.openAccount}
+        onClose={() => closeModal("openAccount")}
+      />
+      <UserModal
+        open={portals.openUser}
+        onClose={() => closeModal("openUser")}
+      />
     </div>
   );
 }
-
-//TODO:
-//Known issues:
-// Its not correct defaulting to index 0 of the chatroom array
-//Not scrolling to the botton on reload
-
-//TODO:
-//Shit I haven't implemented yet
-//Everything
